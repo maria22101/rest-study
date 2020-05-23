41f9b61cc91a665542f8825572195bd5117b7728
Mariia Bilous <maria.bilous101@gmail.com>
2020-05-24 00:44:09 +0300

    project basis

---
+++ src/test/java/com/example/reststudy/RestStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reststudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
